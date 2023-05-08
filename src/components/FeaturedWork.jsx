66cc0e446cc5bd8312b59836a4ea0a53381e1077
v-Alqorith <vahapcandongel@hotmail.com>
2023-05-08 16:49:25 +0100

    reordered

--- src/components/FeaturedWork.jsx
+++ src/components/FeaturedWork.jsx
@@ -22,16 +22,6 @@ export default function FeaturedWork() {
 
         <FeaturedWorkCard
           technologies={["Next JS", "Tailwind CSS", "Supabase"]}
-          projectName={"CGPrompT"}
-          projectDescription={
-            "Designed and created a dynamic web application that is specifically tailored for Chat GPT users. The application enables users to share prompts, view useful prompts posted by other users, rate posted prompts, and provide feedback comments to help enhance the overall user experience."
-          }
-          githubLink={"https://github.com/VahapCanDongel/prompt-engineer"}
-          liveLink={"https://prompt-engineer.vercel.app/"}
-        />
-
-        <FeaturedWorkCard
-          technologies={["Next JS", "Tailwind CSS", "Supabase"]}
           projectName={"Linkteacher"}
           projectDescription={
             "A comprehensive web application designed specifically for teachers to share and distribute lesson materials on a digital board. The platform allows teachers to upload various types of content, such as videos, documents, and images, which can be viewed by learners in designated subjects"
@@ -40,7 +30,15 @@ export default function FeaturedWork() {
           liveLink={"https://vlearn-2.vercel.app/login"}
         />
 
-
+        <FeaturedWorkCard
+          technologies={["Next JS", "Tailwind CSS", "Supabase"]}
+          projectName={"CGPrompT"}
+          projectDescription={
+            "Designed and created a dynamic web application that is specifically tailored for Chat GPT users. The application enables users to share prompts, view useful prompts posted by other users, rate posted prompts, and provide feedback comments to help enhance the overall user experience."
+          }
+          githubLink={"https://github.com/VahapCanDongel/prompt-engineer"}
+          liveLink={"https://prompt-engineer.vercel.app/"}
+        />
       </div>
     </div>
   );
