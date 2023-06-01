0a160f4234102943d18e526fb4c6aeb1d035098d
v-Alqorith <vahapcandongel@hotmail.com>
2023-06-01 18:39:46 +0100

    Changed corresponding to the CV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,20 @@ export default function Hero() {
       <div className="flex justify-center w-1/2 flex-col sm:w-[100%] md:w-[100%]">
         <div className="flex flex-col mr-auto">
           <div className="font-bold text-[40px]">Vahap Can Dongel</div>
-          <div>Front-end Developer / Lecturer in Computing</div>
+          <div>Full Stack Developer</div>
         </div>
 
         <p className="mt-4">
-          As a passionate programmer and computing lecturer, I love bringing
-          ideas to life and sharing my knowledge and experience with students.
-          My love for programming extends beyond teaching, as I enjoy building
-          websites and pushing boundaries to find creative solutions. I'm always
-          excited to tackle new projects and see what I can achieve.
+          Enthusiastic and solutions-oriented Full Stack Developer with
+          hands-on experience in designing, developing, and implementing
+          user-centred web applications. Proficient in a diverse range of
+          technologies including <span className="font-bold italic">JavaScript, React JS, Next JS, Node JS,
+            PostgreSQL, MySQL, </span>and more. Demonstrated experience in
+          efficiently managing application state using and creating visually
+          appealing UI with <span className="font-bold italic">Tailwind CSS</span>. Proven ability in writing
+          comprehensive test cases with <span className="font-bold italic">Jest</span> to ensure software quality
+          and functionality.
+
         </p>
       </div>
     </div>
