c89735de76fc5b97c3ec4ab79ae7f757809b440c
v-Alqorith <vahapcandongel@hotmail.com>
2023-06-01 18:57:34 +0100

    changes corresponding to the cv

--- src/components/FeaturedWork.jsx
+++ src/components/FeaturedWork.jsx
@@ -7,6 +7,15 @@ export default function FeaturedWork() {
         <div className="flex flex-col mr-auto">
           <div className="font-bold text-[27px]">Featured Work</div>
         </div>
+        <FeaturedWorkCard
+          technologies={["Next JS", "Tailwind CSS", "Supabase"]}
+          projectName={"CGPrompT"}
+          projectDescription={
+            "Designed and created a dynamic web application that is specifically tailored for Chat GPT users. The application enables users to share prompts, view useful prompts posted by other users, rate posted prompts, and provide feedback comments to help enhance the overall user experience."
+          }
+          githubLink={"https://github.com/VahapCanDongel/prompt-engineer"}
+          liveLink={"https://prompt-engineer.vercel.app/"}
+        />
 
         <FeaturedWorkCard
           technologies={["React JS", "Tailwind CSS"]}
@@ -28,15 +37,7 @@ export default function FeaturedWork() {
           liveLink={"https://shuishop.vercel.app/"}
         />
 
-        <FeaturedWorkCard
-          technologies={["Next JS", "Tailwind CSS", "Supabase"]}
-          projectName={"CGPrompT"}
-          projectDescription={
-            "Designed and created a dynamic web application that is specifically tailored for Chat GPT users. The application enables users to share prompts, view useful prompts posted by other users, rate posted prompts, and provide feedback comments to help enhance the overall user experience."
-          }
-          githubLink={"https://github.com/VahapCanDongel/prompt-engineer"}
-          liveLink={"https://prompt-engineer.vercel.app/"}
-        />
+
 
       </div>
     </div>
